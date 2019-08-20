d9a0fa2ede49467f7e1b6c01f97f5b06be746ebf
Raghu Betina <raghu@firstdraft.com>
2019-08-20 15:54:23 +0000

    Whitelist columns in CheckIn for ActiveAdmin

--- app/admin/check_ins.rb
+++ app/admin/check_ins.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register CheckIn do
+
+ permit_params :owner_id, :location_id, :image, :caption
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
