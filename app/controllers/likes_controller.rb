c07e2879c85d05e1eb28cc8d794f7b5d2c24e814
Raghu Betina <raghu@firstdraft.com>
2019-08-20 15:52:35 +0000

    Generate likes resource with draft:resource

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,65 @@
+class LikesController < ApplicationController
+  def index
+    @likes = Like.all
+
+    render("like_templates/index.html.erb")
+  end
+
+  def show
+    @like = Like.find(params.fetch("id_to_display"))
+
+    render("like_templates/show.html.erb")
+  end
+
+  def new_form
+    @like = Like.new
+
+    render("like_templates/new_form.html.erb")
+  end
+
+  def create_row
+    @like = Like.new
+
+    @like.author_id = params.fetch("author_id")
+    @like.photo_id = params.fetch("photo_id")
+    @like.location_id = params.fetch("location_id")
+
+    if @like.valid?
+      @like.save
+
+      redirect_back(:fallback_location => "/likes", :notice => "Like created successfully.")
+    else
+      render("like_templates/new_form_with_errors.html.erb")
+    end
+  end
+
+  def edit_form
+    @like = Like.find(params.fetch("prefill_with_id"))
+
+    render("like_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    @like = Like.find(params.fetch("id_to_modify"))
+
+    @like.author_id = params.fetch("author_id")
+    @like.photo_id = params.fetch("photo_id")
+    @like.location_id = params.fetch("location_id")
+
+    if @like.valid?
+      @like.save
+
+      redirect_to("/likes/#{@like.id}", :notice => "Like updated successfully.")
+    else
+      render("like_templates/edit_form_with_errors.html.erb")
+    end
+  end
+
+  def destroy_row
+    @like = Like.find(params.fetch("id_to_remove"))
+
+    @like.destroy
+
+    redirect_to("/likes", :notice => "Like deleted successfully.")
+  end
+end
