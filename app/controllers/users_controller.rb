f7bc8b436769e224f64a5e2863774185a91a5405
Raghu Betina <raghu@firstdraft.com>
2019-08-20 15:52:23 +0000

    Generate users resource with draft:resource

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,69 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+
+    render("user_templates/index.html.erb")
+  end
+
+  def show
+    @user = User.find(params.fetch("id_to_display"))
+
+    render("user_templates/show.html.erb")
+  end
+
+  def new_form
+    @user = User.new
+
+    render("user_templates/new_form.html.erb")
+  end
+
+  def create_row
+    @user = User.new
+
+    @user.email = params.fetch("email")
+    @user.username = params.fetch("username")
+    @user.password_digest = params.fetch("password_digest")
+    @user.image = params.fetch("image")
+    @user.bio = params.fetch("bio")
+
+    if @user.valid?
+      @user.save
+
+      redirect_back(:fallback_location => "/users", :notice => "User created successfully.")
+    else
+      render("user_templates/new_form_with_errors.html.erb")
+    end
+  end
+
+  def edit_form
+    @user = User.find(params.fetch("prefill_with_id"))
+
+    render("user_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    @user = User.find(params.fetch("id_to_modify"))
+
+    @user.email = params.fetch("email")
+    @user.username = params.fetch("username")
+    @user.password_digest = params.fetch("password_digest")
+    @user.image = params.fetch("image")
+    @user.bio = params.fetch("bio")
+
+    if @user.valid?
+      @user.save
+
+      redirect_to("/users/#{@user.id}", :notice => "User updated successfully.")
+    else
+      render("user_templates/edit_form_with_errors.html.erb")
+    end
+  end
+
+  def destroy_row
+    @user = User.find(params.fetch("id_to_remove"))
+
+    @user.destroy
+
+    redirect_to("/users", :notice => "User deleted successfully.")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # Routes for the User resource:
+
+  # CREATE
+  get("/users/new", { :controller => "users", :action => "new_form" })
+  post("/create_user", { :controller => "users", :action => "create_row" })
+
+  # READ
+  get("/users", { :controller => "users", :action => "index" })
+  get("/users/:id_to_display", { :controller => "users", :action => "show" })
+
+  # UPDATE
+  get("/users/:prefill_with_id/edit", { :controller => "users", :action => "edit_form" })
+  post("/update_user/:id_to_modify", { :controller => "users", :action => "update_row" })
+
+  # DELETE
+  get("/delete_user/:id_to_remove", { :controller => "users", :action => "destroy_row" })
+
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
