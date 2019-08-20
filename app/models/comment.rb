95a5df134e317c6a4fca4de5197143718441d2b7
Raghu Betina <raghu@firstdraft.com>
2019-08-20 15:53:13 +0000

    Direct association between photos and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ApplicationRecord
   # Direct associations
 
+  belongs_to :photo,
+             :required => false,
+             :counter_cache => true
+
   belongs_to :author,
              :class_name => "User",
              :counter_cache => true
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,9 @@
 class Photo < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :destroy
+
   belongs_to :author,
              :class_name => "User",
              :counter_cache => true
