d109b5272712e114ae6cde7e3202a15b4ec75962
Raghu Betina <raghu@firstdraft.com>
2019-08-20 15:52:27 +0000

    Generate photos resource with draft:resource

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,71 @@
+class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+
+    render("photo_templates/index.html.erb")
+  end
+
+  def show
+    @photo = Photo.find(params.fetch("id_to_display"))
+
+    render("photo_templates/show.html.erb")
+  end
+
+  def new_form
+    @photo = Photo.new
+
+    render("photo_templates/new_form.html.erb")
+  end
+
+  def create_row
+    @photo = Photo.new
+
+    @photo.author_id = params.fetch("author_id")
+    @photo.comments_count = params.fetch("comments_count")
+    @photo.likes_count = params.fetch("likes_count")
+    @photo.location_id = params.fetch("location_id")
+    @photo.caption = params.fetch("caption")
+    @photo.image = params.fetch("image")
+
+    if @photo.valid?
+      @photo.save
+
+      redirect_back(:fallback_location => "/photos", :notice => "Photo created successfully.")
+    else
+      render("photo_templates/new_form_with_errors.html.erb")
+    end
+  end
+
+  def edit_form
+    @photo = Photo.find(params.fetch("prefill_with_id"))
+
+    render("photo_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    @photo = Photo.find(params.fetch("id_to_modify"))
+
+    @photo.author_id = params.fetch("author_id")
+    @photo.comments_count = params.fetch("comments_count")
+    @photo.likes_count = params.fetch("likes_count")
+    @photo.location_id = params.fetch("location_id")
+    @photo.caption = params.fetch("caption")
+    @photo.image = params.fetch("image")
+
+    if @photo.valid?
+      @photo.save
+
+      redirect_to("/photos/#{@photo.id}", :notice => "Photo updated successfully.")
+    else
+      render("photo_templates/edit_form_with_errors.html.erb")
+    end
+  end
+
+  def destroy_row
+    @photo = Photo.find(params.fetch("id_to_remove"))
+
+    @photo.destroy
+
+    redirect_to("/photos", :notice => "Photo deleted successfully.")
+  end
+end
